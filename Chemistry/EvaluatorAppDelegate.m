da3881a67a5fa7e9e8de818dd628e5dda6cdf4b2
Joseph caxton-Idowu <Joseph@Joseph-caxton-Idowus-iMac.local>
2012-07-04 11:35:24 +0100

    Review

--- Chemistry/EvaluatorAppDelegate.m
+++ Chemistry/EvaluatorAppDelegate.m
@@ -172,6 +172,26 @@ static NSString* const kAnalyticsAccountId = @"UA-31971143-1";
         NSLog(@"error in trackPageview");
     }
 
+    // Review
+    NSUserDefaults *prefs = [NSUserDefaults standardUserDefaults];
+    NSString *ReviewID = [prefs stringForKey:@"Review"];
+    if (ReviewID == nil) {
+        
+        NSString *ID = @"0";
+        [prefs setObject:ID forKey:@"Review"];
+        [prefs setObject:ID forKey:@"IHaveLeftReview"];
+        
+        [prefs synchronize];
+        
+    }
+    //for testing
+    /*NSString *ID = @"0";
+     [prefs setObject:ID forKey:@"Review"];
+     [prefs setObject:ID forKey:@"IHaveLeftReview"];
+     
+     [prefs synchronize];*/
+    
+
     
 	return YES;
 }
